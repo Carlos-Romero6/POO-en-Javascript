81789296a15ea17945e518e9d9d51fcddce8c1af
Carlos-Romero6 <carloshernanromerogonzalez@gmail.com>
2024-11-16 14:25:42 -0400

    Add files via upload
    Sintaxis de declaración de clases y objetos.

---
+++ Introducción a la POO/Clases-y-objetos.js
@@ -0,0 +1,17 @@
+class LaClase {
+    constructor(parame1, parame2){
+        //Atributos
+        this.parame1 = parame1;
+        this.parame2 = parame2; 
+    }
+    metodo(){ //Declaración de un método
+        console.log("Este objeto está reallizando un método...")
+    }
+    
+}
+
+let objeto = new LaClase("parame1","parame2")
+
+objeto.metodo //La llamada del método
+
+
